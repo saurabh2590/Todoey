b4d856e40e23bc23a6f270996c37078c8942f165
saurabh2590@gmail.com <saurabh2590@gmail.com>
2018-08-28 22:29:55 +0200

    Using User defaults to persist the user saved todo list

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -12,9 +12,13 @@ class TodoListViewController: UITableViewController {
     
     var itemArray = ["Find Mike", "Buy a car", "Call Jenny"]
 
+    let defaults = UserDefaults.standard
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+            itemArray = items
+        }
     }
 
 //    override func didReceiveMemoryWarning() {
@@ -52,6 +56,7 @@ class TodoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             // once user clicks add item on the alert
             self.itemArray.append(textField.text!)
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
             self.tableView.reloadData()
         }
         alert.addTextField { (alertTextField) in
